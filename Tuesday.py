883879ed7fa7a6f76bfbb703d37fed192a369527
Mark Kelly <G00364705@gmit.ie>
2018-10-01 22:04:00 +0100

    Committing final Tuesday program

--- Tuesday.py
+++ Tuesday.py
@@ -2,7 +2,16 @@ import datetime
 today = datetime.datetime.today()
 dayofweek = today.weekday()
 
-if dayofweek == 2:
+print("lets check if it's Tuesday")
+
+if dayofweek == 1:
   print("It's Tuesday!")
+elif dayofweek == 0:
+  print("It will be Tuesday tomorrow")
+elif dayofweek ==2:
+  print("Ah, it was Tuesday yesterday!")
 else:
   print("Sorry, it's not Tuesday")
+
+
+print("Thanks for checking!")
