e835a4bdcef4f5c6739448a52eaf64517212f0c7
Mark Kelly <G00364705@gmit.ie>
2018-10-02 21:47:04 +0100

    squares using a for loop

---
+++ squares.py
@@ -0,0 +1,13 @@
+# Dealing with For Loops
+# Mark Kelly S2 - wk1 Recap
+
+#l = [0,1,2,3,4,5,6,7,8,9]
+
+# instead of setting the intial list a range could be used.
+#for i in l:
+for i in range(100):
+  #print(i, i*i)
+  print(f"{i:3} {i*i:5}")
+  #prints i and the square of i. i has 3 character spaces while i*i has 5. f formats the output++++
+
+print("Thats the full list")
